379da682af89a0f13a34c85f88f159867019282d
Pyotr Ermishkin <quasiyoke@gmail.com>
2013-10-25 06:49:44 +0400

    Sass compilation during build added.

--- setup.py
+++ setup.py
@@ -2,6 +2,24 @@
 
 from setuptools import setup
 
+import os
+SETUP_DIR = os.path.dirname(os.path.abspath(__file__))
+os.chdir(os.path.join(SETUP_DIR, 'chat'))
+
+import platform
+if 'Windows' == platform.system():
+    command = 'compass.bat compile'
+else:
+    command = 'compass compile'
+
+import subprocess
+try:
+    subprocess.check_call(command.split())
+except (subprocess.CalledProcessError, OSError):
+    print 'ERROR: problems with compiling Sass. Is Compass installed?'
+    raise SystemExit
+os.chdir(SETUP_DIR)
+    
 setup(
     name='tornado_chat',
     version='0.0.1',
@@ -11,7 +29,7 @@ setup(
     url='https://github.com/quasiyoke/tornado-chat',
     packages=['chat', ],
     package_data={
-        'chat': ['templates/*.html', ],
+        'chat': ['templates/*.html', 'static/css/*.css', 'static/js/*.js', ],
     },
     install_requires=['tornado', ],
 )
