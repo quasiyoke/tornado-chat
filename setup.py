5edf78a73a658df142738862b74214f5624a79c1
Pyotr Ermishkin <quasiyoke@gmail.com>
2013-10-25 04:09:01 +0400

    setup.py and .gitignore added.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='tornado_chat',
+    version='0.0.1',
+    description='Yet another simple chat example on tornado framework.',
+    author='Pyotr Ermishkin',
+    author_email='quasiyoke@gmail.com',
+    url='https://github.com/quasiyoke/tornado-chat',
+    packages=['chat', ],
+    install_requires=['tornado', ],
+)
