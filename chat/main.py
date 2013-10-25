114eca42ba3d208d994be77a5d41e7966bf6390d
Pyotr Ermishkin <quasiyoke@gmail.com>
2013-10-25 11:36:33 +0400

    Users' presence notifications through websocket added.

--- chat/main.py
+++ chat/main.py
@@ -1,8 +1,10 @@
 import colors
+import json
 from os import path
 from tornado import ioloop
 from tornado import template
 from tornado import web
+from tornado import websocket
 
 PROJECT_PATH = path.dirname(__file__)
 
@@ -14,8 +16,52 @@ class Main(web.RequestHandler):
         self.write(template_loader.load('main.html').generate(color=colors.get_random_color()))
 
 
+class Socket(websocket.WebSocketHandler):
+    def open(self):
+        sockets.append(self)
+
+    def on_message(self, message):
+        message = json.loads(message)
+        if message['type'] == 'set_color':
+            self.color = message['color']
+            sockets.write_message({
+                'type': 'user_come',
+                'color': self.color
+            }, exc=self)
+        elif not hasattr(self, 'color'):
+            return
+            
+
+    def on_close(self):
+        sockets.remove(self)
+
+
+class Sockets(object):
+    _sockets = []
+
+    def write_message(self, message, exc=None):
+        for socket in self._sockets:
+            if socket != exc:
+                socket.write_message(message)
+
+    def append(self, socket):
+        self._sockets.append(socket)
+
+    def remove(self, socket):
+        self._sockets.remove(socket)
+        message = {
+            'type': 'user_gone',
+        }
+        if hasattr(socket, 'color'):
+            message['color'] = socket.color
+        self.write_message(message)
+
+sockets = Sockets()
+
+
 application = web.Application([
     (ur'/', Main),
+    (ur'/socket/', Socket),
     (ur'/static/(.*)', web.StaticFileHandler, {'path': path.join(PROJECT_PATH, 'static')}),    
 ])
 
--- chat/static/js/main.js
+++ chat/static/js/main.js
@@ -1,10 +1,34 @@
 (function(){
+	var domainPort = /\w+:\/\/([\w\.\:\d]+)/.exec(document.documentURI)[1];
+	var socket = new WebSocket('ws://' + domainPort + '/socket/');
+	socket.onopen = function(){
+		socket.send(JSON.stringify({
+			type: 'set_color',
+			color: COLOR
+		}));
+	};
+	
 	window.addEventListener('load', function(){
-		var body = document.getElementsByTagName('body')[0];
-		var bodyWidth = body.getClientRects()[0].width;
+		var bodyWidth = document.body.getClientRects()[0].width;
 
 		var formWrap = document.getElementsByClassName('form-wrap')[0];
 		var formWrapWidth = formWrap.getClientRects()[0].width;
-		formWrap.style.left = (bodyWidth - formWrapWidth) / 2 + 'px';		
+		formWrap.style.left = (bodyWidth - formWrapWidth) / 2 + 'px';
+
+		var messages = document.getElementsByClassName('messages')[0];
+		
+		socket.onmessage = function(e){
+			var message = JSON.parse(e.data);
+			var text;
+			if('user_come' === message.type){
+				text = 'Meet new user';
+			}else if('user_gone' === message.type){
+				text = 'User gone';
+			}
+			var el = document.createElement('li');
+			el.appendChild(document.createTextNode(text));
+			el.style.color = '#' + message.color;
+			messages.appendChild(el);
+		};
 	});
 })();
